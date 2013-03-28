ff6d2ba705110436424042cbb1be42911af01732
Jez Higgins <jez@jezuk.co.uk>
2013-03-28 12:51:41 +0000

    Revert "Corrected license key"
    This reverts commit 849a8cc00e1c002e42b5d387d84e7a7202586368.

--- czprague/src/net/cyclestreets/maps/czprague/MapFileDownloaderService.java
+++ czprague/src/net/cyclestreets/maps/czprague/MapFileDownloaderService.java
@@ -9,12 +9,12 @@ import com.google.android.vending.expansion.downloader.impl.DownloaderService;
 public class MapFileDownloaderService extends DownloaderService
 {
    // the Base64-encoded RSA public key for your publisher account
-   private static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAifqFvadouMWeZnGj9LQYNRliBcI0jfbN9N/hSrQAuerRDIaykbxrlnIFRE0TNZM/zbkx/38m/x3WXlZpiZ5hKaNHHeTgTUUGhFDserVKdNfwXKhi/G4YLu9v+2F+H9/XjtEEHB39PqjyGZwKCnED6Q69643YcOGopFfSQ2rfoDQClBuLvVp3VjwXwxryOsbXeJ1fEGKuZelAz6Sxo84inf4ZvS12S4JydfkR1ZN0S21Ai0eFlrY3huCq076D1Jibrstp863EgBX7On39dC5IDDKqFW0/3XcLB94cC5ukcKIfmc403vT+hvyxe1DYsd4TxdRcpY3TDFC/Lm7feSlJ2QIDAQAB";
+   private static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh/VB2RFxiXs1ZDuKndP6+qMJAXOrrhSG8tiNYxuK6gUz5gIkCgC6/ARWZbO522ukm1J+aizEAp+mgdZ/JQ2f34nZ6kYmmgfqZ8KfRVN81gv6nltqIyk1Eb4Nn1rlPu27zvSfRjjAUJOVbsCOL4iMcMguoMUZy6TtN41uTw5YvPRtV1hLNOrdefM/zJpqa2L+6Wmy9ARpbhdTE8PmmGLGKOVcnhOoZyzsAJMHVBjX9NU72bVgjMoLxaNbw84lMPqZCi2jm0grg4jjPLsoIb3QzVh8PNnIqIXj7zLuvarbuxyKxbDS1QNwpvHhqnmkTGJedlGFtdA7L9ms1v1piJax9wIDAQAB";
    // Generate 20 random bytes, and put them here.
-   private static final byte[] SALT = new byte[] { 125, 87, 122, 18, 99,
-                                                   -12, 87, 13, 12, 5,
+   private static final byte[] SALT = new byte[] { 125, 87, 122, 18, 88,
+                                                   -12, 43, 13, 12, 5,
                                                    112, 74, 19 -2, -1,
-                                                   12, 33, 48, 12, 63 };
+                                                   12, 33, 48, 12, 12 };
 
    @Override
    protected String getPublicKey() {
