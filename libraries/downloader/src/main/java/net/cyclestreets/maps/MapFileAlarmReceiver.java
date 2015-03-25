51edc482027b0c583864b4b814dc386e983d7acd
Jez Higgins <jez@jezuk.co.uk>
2015-03-25 20:08:11 +0000

    start pulling out library

---
+++ libraries/downloader/src/main/java/net/cyclestreets/maps/MapFileAlarmReceiver.java
@@ -0,0 +1,26 @@
+package net.cyclestreets.maps;
+
+import com.google.android.vending.expansion.downloader.DownloaderClientMarshaller;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
+
+public class MapFileAlarmReceiver extends BroadcastReceiver 
+{
+  @Override 
+  public void onReceive(Context context, Intent intent) 
+  {
+    try 
+    {
+      DownloaderClientMarshaller.startDownloadServiceIfRequired(
+         context, intent, MapFileDownloaderService.class);
+    } 
+    catch (NameNotFoundException e) 
+    {
+      Log.e("apk-expansion-files", "NameNotFoundException occurred. " + e.getMessage(), e);
+    }
+  } // onReceive
+} // class MapFileAlarmReceiver
